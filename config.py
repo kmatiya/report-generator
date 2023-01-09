31fd9685301b027943742f2d52648162e10be3a8
kmatiya <khormatiya@gmail.com>
2023-01-09 06:49:22 +0200

    Add configuration file

---
+++ config.py
@@ -0,0 +1,27 @@
+config = {
+    "endpoint": [{
+        "base": "http://localhost",
+        "username": "test",
+        "password": "test",
+        "default_start_date": "",
+        "default_end_date": "",
+        "locations": [
+
+        ]
+    },
+    ],
+    "reports": [
+        {
+            "resource": "/openmrs/ws/rest/v1/pihmalawi/report/test",
+            "prefix": "",
+            "name": "",
+            "use_start_date_in_request": True,
+            "use_end_date_in_request": True,
+            "directory": "",
+            "column_names": [
+
+            ]
+        }
+    ]
+
+}
