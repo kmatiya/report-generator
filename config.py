3c666c64960fb17401857a2d577089491bf93a79
kmatiya <khormatiya@gmail.com>
2023-01-12 05:30:38 +0200

    Update config to include base file path

--- config.py
+++ config.py
@@ -1,27 +1,27 @@
 config = {
-    "endpoint": [{
-        "base": "http://localhost",
-        "username": "test",
-        "password": "test",
-        "default_start_date": "",
-        "default_end_date": "",
-        "locations": [
-
+    "base_file_path": "./Q4 Reports",
+    "endpoints": [{
+        "base": "http://localhost:8081/openmrs/ws/rest/v1/",
+        "username": "",
+        "password": "",
+        "default_start_date": "2022-04-01",
+        "default_end_date": "2022-06-30",
+        "location": [
+            "Neno District Hospital", "Ligowe HC"
         ]
     },
     ],
     "reports": [
         {
-            "resource": "/openmrs/ws/rest/v1/pihmalawi/report/test",
-            "prefix": "",
-            "name": "",
+            "resource": "pihmalawi/report/moh-regimen-switch",
+            "prefix": "MoH",
+            "name": "Regimen Switch Report",
             "use_start_date_in_request": True,
-            "use_end_date_in_request": True,
+            "use_end_date_in_request": False,
             "directory": "",
             "column_names": [
 
             ]
-        }
+        },
     ]
-
 }
