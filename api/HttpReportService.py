378658a81f660775e5c2e1604b362d66a3931901
kmatiya <khormatiya@gmail.com>
2023-01-12 05:29:07 +0200

    Update HttpReportService class to update each report with start date and end date

--- api/HttpReportService.py
+++ api/HttpReportService.py
@@ -12,20 +12,30 @@ class HttpReportService:
     def get_reports_from_server(self):
         for each_endpoint in self.__config["endpoints"]:
             reports = []
+            start_date = each_endpoint['default_start_date']
+            end_date = each_endpoint['default_end_date']
             for each_report in self.__config["reports"]:
                 report_for_all_locations = []
                 for each_location in each_endpoint["location"]:
+                    location = each_location
                     url = each_endpoint["base"] + each_report["resource"]
                     params = {
-                        # "startDate": each_endpoint["default_start_date"],
-                        "date": each_endpoint["default_end_date"],
-                        "location": each_location
+                        "location": location
                     }
+                    if each_report['use_start_date_in_request']:
+                        params['startDate'] = start_date
+                    if each_report['use_end_date_in_request']:
+                        params['endDate'] = end_date
                     get_report = requests.get(url, params=params,
                                               auth=HTTPBasicAuth(username=each_endpoint["username"],
                                                                  password=each_endpoint["password"]))
                     if get_report.status_code == 200:
-                        report_for_all_locations.append(json.loads(get_report.text))
+                        report_json = json.loads(get_report.text)
+                        for e in report_json:
+                            e["facility_name"] = location
+                            e['start_date'] = start_date
+                            e['end_date'] = end_date
+                        report_for_all_locations.append(report_json)
                 reports.append(
                     {
                         each_report["name"]: report_for_all_locations
