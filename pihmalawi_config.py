57c4c96bfda6ee02a58009118a016770107225ee
kmatiya <khormatiya@gmail.com>
2023-01-14 16:56:44 +0200

    Add all Partners in Health MoH and PEPFAR reports configuration

--- pihmalawi_config.py
+++ pihmalawi_config.py
@@ -8,7 +8,8 @@ config = {
             "default_start_date": "2022-04-01",
             "default_end_date": "2022-06-30",
             "location": [
-                "Neno District Hospital", "Ligowe HC"
+                "Neno District Hospital", "Ligowe HC", "Magaleta HC", "Nsambe HC", "Dambe Clinic", "Neno Mission HC",
+                "Matandani Rural Health Center", "Luwani RHC"
             ]
         },
     ],
@@ -98,6 +99,186 @@ config = {
             ],
             "columns_to_remove": [
                 'reportingYear', 'reportingQuarter', 'facility_name', 'start_date', 'end_date'
+            ]
+        },
+        {
+            "resource": "pihmalawi/report/moh-regimen-dispensation",
+            "prefix": "MoH",
+            "name": "Regimen Dispensation Report",
+            "use_start_date_in_request": False,
+            "use_end_date_in_request": True,
+            "column_names_to_rename": [
+                {"identifier": "ARV#"},
+                {"gender": "Gender"},
+                {"dob": "DOB"},
+                {"artStartDate": "ART start date"},
+                {"weight": "Weight(Kg)"},
+                {"artRegimen": "Regimen"},
+                {"arvsGiven": "ARVs"},
+                {"dispenseDate": "Dispensed date"},
+            ],
+            "columns_to_remove": [
+                'location', 'facility_name', 'start_date', 'end_date'
+            ]
+        },
+        {
+            "resource": "pihmalawi/report/moh-regimen-dispensation-by-weight",
+            "prefix": "MoH",
+            "name": "Regimen Dispensation By Weight Report",
+            "use_start_date_in_request": False,
+            "use_end_date_in_request": True,
+            "column_names_to_rename": [
+                # {"identifier": "ARV#"},
+                # {"gender": "Gender"},
+                # {"dob": "DOB"},
+                # {"artStartDate": "ART start date"},
+                # {"weight": "Weight(Kg)"},
+                # {"artRegimen": "Regimen"},
+                # {"arvsGiven": "ARVs"},
+                # {"dispenseDate": "Dispensed date"},
+            ],
+            "columns_to_remove": [
+                # 'location', 'facility_name', 'start_date', 'end_date'
+            ]
+        },
+        {
+            "resource": "pihmalawi/report/moh-cohort-disaggregated",
+            "prefix": "MoH",
+            "name": "Cohort Disaggregated Report",
+            "use_start_date_in_request": True,
+            "use_end_date_in_request": True,
+            "column_names_to_rename": [
+                # {"identifier": "ARV#"},
+                # {"gender": "Gender"},
+                # {"dob": "DOB"},
+                # {"artStartDate": "ART start date"},
+                # {"weight": "Weight(Kg)"},
+                # {"artRegimen": "Regimen"},
+                # {"arvsGiven": "ARVs"},
+                # {"dispenseDate": "Dispensed date"},
+            ],
+            "columns_to_remove": [
+                # 'location', 'facility_name', 'start_date', 'end_date'
+            ]
+        },
+        {
+            "resource": "pihmalawi/report/moh-tpt-new-initiation",
+            "prefix": "MoH",
+            "name": "TPT New Initiation Report",
+            "use_start_date_in_request": True,
+            "use_end_date_in_request": True,
+            "column_names_to_rename": [
+                # {"identifier": "ARV#"},
+                # {"gender": "Gender"},
+                # {"dob": "DOB"},
+                # {"artStartDate": "ART start date"},
+                # {"weight": "Weight(Kg)"},
+                # {"artRegimen": "Regimen"},
+                # {"arvsGiven": "ARVs"},
+                # {"dispenseDate": "Dispensed date"},
+            ],
+            "columns_to_remove": [
+                # 'location', 'facility_name', 'start_date', 'end_date'
+            ]
+        },
+        {
+            "resource": "pihmalawi/report/moh-tx-curr-mmd",
+            "prefix": "MoH",
+            "name": "TX_CURR MMD Report",
+            "use_start_date_in_request": True,
+            "use_end_date_in_request": True,
+            "column_names_to_rename": [
+                # {"identifier": "ARV#"},
+                # {"gender": "Gender"},
+                # {"dob": "DOB"},
+                # {"artStartDate": "ART start date"},
+                # {"weight": "Weight(Kg)"},
+                # {"artRegimen": "Regimen"},
+                # {"arvsGiven": "ARVs"},
+                # {"dispenseDate": "Dispensed date"},
+            ],
+            "columns_to_remove": [
+                # 'location', 'facility_name', 'start_date', 'end_date'
+            ]
+        },
+        {
+            "resource": "pihmalawi/report/pepfar-cohort-disaggregated",
+            "prefix": "PEPFAR",
+            "name": "Cohort Disaggregated Report",
+            "use_start_date_in_request": True,
+            "use_end_date_in_request": True,
+            "column_names_to_rename": [
+                # {"identifier": "ARV#"},
+                # {"gender": "Gender"},
+                # {"dob": "DOB"},
+                # {"artStartDate": "ART start date"},
+                # {"weight": "Weight(Kg)"},
+                # {"artRegimen": "Regimen"},
+                # {"arvsGiven": "ARVs"},
+                # {"dispenseDate": "Dispensed date"},
+            ],
+            "columns_to_remove": [
+                # 'location', 'facility_name', 'start_date', 'end_date'
+            ]
+        },
+        {
+            "resource": "pihmalawi/report/pepfar-cohort-disaggregated",
+            "prefix": "PEPFAR",
+            "name": "TX_CURR MMD Report",
+            "use_start_date_in_request": True,
+            "use_end_date_in_request": True,
+            "column_names_to_rename": [
+                # {"identifier": "ARV#"},
+                # {"gender": "Gender"},
+                # {"dob": "DOB"},
+                # {"artStartDate": "ART start date"},
+                # {"weight": "Weight(Kg)"},
+                # {"artRegimen": "Regimen"},
+                # {"arvsGiven": "ARVs"},
+                # {"dispenseDate": "Dispensed date"},
+            ],
+            "columns_to_remove": [
+                # 'location', 'facility_name', 'start_date', 'end_date'
+            ]
+        },
+        {
+            "resource": "pihmalawi/report/pepfar-tx-ml",
+            "prefix": "PEPFAR",
+            "name": "TX_ML Report",
+            "use_start_date_in_request": True,
+            "use_end_date_in_request": True,
+            "column_names_to_rename": [
+                # {"identifier": "ARV#"},
+                # {"gender": "Gender"},
+                # {"dob": "DOB"},
+                # {"artStartDate": "ART start date"},
+                # {"weight": "Weight(Kg)"},
+                # {"artRegimen": "Regimen"},
+                # {"arvsGiven": "ARVs"},
+                # {"dispenseDate": "Dispensed date"},
+            ],
+            "columns_to_remove": [
+                # 'location', 'facility_name', 'start_date', 'end_date'
+            ]
+        },
+        {
+            "resource": "pihmalawi/report/pepfar-tx-rtt",
+            "prefix": "PEPFAR",
+            "name": "TX_RTT Report",
+            "use_start_date_in_request": True,
+            "use_end_date_in_request": True,
+            "column_names_to_rename": [
+                # {"identifier": "ARV#"},
+                # {"gender": "Gender"},
+                # {"dob": "DOB"},
+                # {"artStartDate": "ART start date"},
+                # {"weight": "Weight(Kg)"},
+                # {"artRegimen": "Regimen"},
+                # {"arvsGiven": "ARVs"},
+                # {"dispenseDate": "Dispensed date"},
+            ],
+            "columns_to_remove": [
+                # 'location', 'facility_name', 'start_date', 'end_date'
             ]
         },
     ]
